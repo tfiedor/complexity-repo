d8e5818fe7b255f9f4e02c8e59d08afc4a8205f2
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2017-10-24 14:55:20 +0200

    Fix local.yml
    local.yml: Fix incorrect parameter name;
    workload: slight improvement.

--- cpp_sources/workload/main.cpp
+++ cpp_sources/workload/main.cpp
@@ -1,28 +1,41 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "SLList.h"
 #include "SLListcls.h"
 #include "../profile_api.h"
 
+#define R_MIN 1
+#define R_MAX 100000
+#define R_RANGE(min, max) (rand() % ((R_MAX) - (R_MIN)) + (R_MIN))
+
 int main() {
+
+    srand(time(NULL));
 
     SLList mylist;
     _profapi_register_size_address(&mylist, false, &mylist.size);
     SLList_init(&mylist);
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 200; ++i) {
         _profapi_using_size_address(&mylist);
-        SLList_insert(&mylist, i + 1);
+        int inserted_item = i + 1;
+        if(rand() % 3 == 0) {
+            SLList_insert(&mylist, inserted_item);
+            _profapi_using_size_address(&mylist);
+        } else {
+            SLList_search(&mylist, inserted_item);
+            _profapi_using_size_address(&mylist);
+        }
     }
-    _profapi_using_size_address(&mylist);
-    SLList_search(&mylist, 3);
     _profapi_using_size_address(&mylist);
     SLList_destroy(&mylist);
 
-    SLListcls clslist;
-    for(int i = 0; i < 10; i++) {
-        clslist.Insert(i + 1);
-    }
-    clslist.Search(3);
-
-
+//    SLListcls clslist;
+//    for(int i = 0; i < 100000; i++) {
+//        clslist.Insert(i + 1);
+//    }
+//    clslist.Search(3);
 
     return 0;
 }
